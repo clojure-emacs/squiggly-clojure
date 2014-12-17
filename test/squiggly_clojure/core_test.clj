eb1d9d58bf2ce69117c896349ed6cdb806795264
Peter Fraenkel <pnf@podsnap.com>
2014-12-17 10:16:44 -0500

    Begin bundling in Clojure code

---
+++ test/squiggly_clojure/core_test.clj
@@ -0,0 +1,7 @@
+(ns squiggly-clojure.core-test
+  (:require [clojure.test :refer :all]
+            [squiggly-clojure.core :refer :all]))
+
+(deftest a-test
+  (testing "FIXME, I fail."
+    (is (= 0 1))))
