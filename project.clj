38de9a47b22f7cca129170c409e988b34a49ec4d
Peter Fraenkel <pnf@podsnap.com>
2015-01-08 17:24:52 -0500

    Upgrade to core.typed 2.77

--- project.clj
+++ project.clj
@@ -5,7 +5,7 @@
             :url "http://www.eclipse.org/legal/epl-v10.html"}
   :dependencies [[org.clojure/clojure "1.6.0"]
                  [environ "1.0.0"]
-                 [org.clojure/core.typed "0.2.72"]
+                 [org.clojure/core.typed "0.2.77"]
                  [jonase/eastwood "0.2.1" :exclusions [org.clojure/clojure]]
                  [jonase/kibit "0.0.8"]]
 
