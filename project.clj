9fe3ea8aee9b75046dc752e2cd85df1fdac86e15
Peter Fraenkel <pnf@podsnap.com>
2016-12-31 11:14:12 -0500

    Upgrade to kibit 0.13, eastwood 0.2.3, typed 0.3.26 -> squiggly 0.1.7

--- project.clj
+++ project.clj
@@ -1,14 +1,14 @@
-(defproject acyclic/squiggly-clojure "0.1.6"
+(defproject acyclic/squiggly-clojure "0.1.7"
   :description "Flycheck for Clojure"
   :url "http://example.com/FIXME"
   :license {:name "Eclipse Public License"
             :url "http://www.eclipse.org/legal/epl-v10.html"}
   :dependencies [[org.clojure/clojure "1.7.0"]
                  [environ "1.0.0"]
-                 [org.clojure/core.typed "0.3.7"]
+                 [org.clojure/core.typed "0.3.26"]
                  [org.clojure/data.json "0.2.6"]
-                 [jonase/eastwood "0.2.1" :exclusions [org.clojure/clojure]]
-                 [jonase/kibit "0.1.2"]]
+                 [jonase/eastwood "0.2.3" :exclusions [org.clojure/clojure]]
+                 [jonase/kibit "0.1.3"]]
 
 
   :deploy-repositories [["clojars" :clojars]
