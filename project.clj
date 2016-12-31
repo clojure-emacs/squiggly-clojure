254bd6a65a262896a4cc4f2061a6bbce51a5b73f
Peter Fraenkel <pnf@podsnap.com>
2016-12-31 11:36:39 -0500

    exclusions, and elisp version matches now

--- project.clj
+++ project.clj
@@ -3,9 +3,9 @@
   :url "http://example.com/FIXME"
   :license {:name "Eclipse Public License"
             :url "http://www.eclipse.org/legal/epl-v10.html"}
-  :dependencies [[org.clojure/clojure "1.7.0"]
+  :dependencies [[org.clojure/clojure "1.8.0"]
                  [environ "1.0.0"]
-                 [org.clojure/core.typed "0.3.26"]
+                 [org.clojure/core.typed "0.3.26" :exclusions [org.clojure/clojure]]
                  [org.clojure/data.json "0.2.6"]
                  [jonase/eastwood "0.2.3" :exclusions [org.clojure/clojure]]
                  [jonase/kibit "0.1.3"]]
