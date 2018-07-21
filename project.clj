d81d875f83f7db26cd7bf43d0f9bab272fb54a3c
Peter Fraenkel <pnf@podsnap.com>
2018-07-21 11:17:00 -0400

    update all dependencies

--- project.clj
+++ project.clj
@@ -3,12 +3,12 @@
   :url "http://example.com/FIXME"
   :license {:name "Eclipse Public License"
             :url "http://www.eclipse.org/legal/epl-v10.html"}
-  :dependencies [[org.clojure/clojure "1.8.0"]
+  :dependencies [[org.clojure/clojure "1.9.0"]
                  [environ "1.1.0"]
-                 [org.clojure/core.typed "0.3.32" :exclusions [org.clojure/clojure] :classifier "slim"]
+                 [org.clojure/core.typed "0.5.3" :exclusions [org.clojure/clojure] :classifier "slim"]
                  [org.clojure/data.json "0.2.6"]
-                 [jonase/eastwood "0.2.3" :exclusions [org.clojure/clojure]]
-                 [jonase/kibit "0.1.3"]]
+                 [jonase/eastwood "0.2.8" :exclusions [org.clojure/clojure]]
+                 [jonase/kibit "0.1.6"]]
 
 
   :deploy-repositories [["clojars" :clojars]
@@ -19,4 +19,3 @@
         :url "https://github.com/pnf/squiggly-clojure"}
 
   )
-
--- sample-project/project.clj
+++ sample-project/project.clj
@@ -3,15 +3,22 @@
   :url "http://example.com/FIXME"
   :license {:name "Eclipse Public License"
             :url "http://www.eclipse.org/legal/epl-v10.html"}
-  :dependencies [[org.clojure/clojure "1.8.0"]
-                 [org.clojure/core.typed "0.3.32"  :classifier "slim"]]
+  :dependencies [[org.clojure/clojure "1.9.0"]
+                 [org.clojure/core.typed "0.5.3"  :classifier "slim"]]
 
   ;; Use lein-environ plugin if you want to set checker configuration in profile.
-  :plugins [[lein-environ "1.0.0"]]
+  :plugins [[lein-environ "1.1.0"]]
 
   ;; Configuration here may be overridden by namespace metadata.
   :profiles {:dev {:env {:squiggly {:checkers [:eastwood :typed]
                                     :eastwood-exclude-linters [:unlimited-use]
                                     :eastwood-options {;; :builtin-config-files ["myconfigfile.clj"]
-                                                       }}}}})
 
+                                                       }}}}}
+
+  ;; This is normally required for automatic type-checking, but it is not necessary
+  ;; when clojure-flycheck is invoking the type checker explicitly.  (Moreover, the
+  ;; injection conflcits with eastwood.)
+  ;;:injections [(require 'clojure.core.typed)       (clojure.core.typed/install)]
+
+  )
