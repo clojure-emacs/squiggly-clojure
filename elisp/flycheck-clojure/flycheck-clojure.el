1dc8fdd691bcf950e95a0f00b71b53854923543e
Peter Fraenkel <pnf@podsnap.com>
2019-06-11 19:51:51 -0400

    update package-requires header

--- elisp/flycheck-clojure/flycheck-clojure.el
+++ elisp/flycheck-clojure/flycheck-clojure.el
@@ -8,7 +8,7 @@
 ;; Maintainer: Peter Fraenkel <pnf@podsnap.com>
 ;; URL: https://github.com/clojure-emacs/squiggly-clojure
 ;; Version: 1.8.0
-;; Package-Requires: ((cider "0.8.1") (flycheck "0.22-cvs1") (let-alist "1.0.1") (emacs "24"))
+;; Package-Requires: ((cider "0.22.0") (flycheck "32-cvs") (let-alist "1.0.1") (emacs "25"))
 
 ;; This file is not part of GNU Emacs.
 
